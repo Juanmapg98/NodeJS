d60cba7f6e364cde011401766411df5e86b805fe
Juan Palacios <juan980424@gmail.com>
2020-08-12 14:45:18 -0500

    informacion del body, query y leer y crear headers

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const router = express.Router();
-var app = express();
 
+var app = express();
+app.use(bodyParser.json());
+// El router debe ir siempre al final
 app.use(router);
 
 router.get('/message', function(req, res){
+    res.header({
+        "custom-header": "holix baby"
+    })
+    console.log(req.headers["custom-header"])
     res.send('Lista de mensajes')
 })
 
 router.post('/message', function(req,res){
+    console.log(req.query)
+    console.log(req.body)
     res.send('El mensaje ha sido añadido')
 })
-// app.use('/', function(req,res) {
-//     res.send('Hola');
-// });
 
 app.listen(3000);
 console.log('la aplicacion esta escuchando en http://localhost:3000')
