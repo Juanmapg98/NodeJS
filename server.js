6d3fc6f5a878a3a5315952c0d7507e3cdd129887
Juan Palacios <juan980424@gmail.com>
2020-08-12 12:06:59 -0500

    manejo de rutas y metodos con el uso del router de express

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
-
+const router = express.Router();
 var app = express();
 
-app.use('/', function(req,res) {
-    res.send('Hola');
-});
+app.use(router);
+
+router.get('/message', function(req, res){
+    res.send('Lista de mensajes')
+})
+
+router.post('/message', function(req,res){
+    res.send('El mensaje ha sido añadido')
+})
+// app.use('/', function(req,res) {
+//     res.send('Hola');
+// });
 
 app.listen(3000);
 console.log('la aplicacion esta escuchando en http://localhost:3000')
