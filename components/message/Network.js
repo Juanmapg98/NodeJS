1d0dce2ea2c17183c9cd0277eea3d8e964a62f1d
Juan Palacios <juan980424@gmail.com>
2020-08-12 23:10:18 -0500

    definiendo logica de negocio

--- components/message/Network.js
+++ components/message/Network.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const router = express.Router();
 const response = require('../../network/response')
-
-router.get('/', function(req, res){
-    response.success(req, res, 'tareas obtenidas',200)
+const controller = require('./Controller')
+const router = express.Router();
+router.get('/', function (req, res) {
+    response.success(req, res, 'tareas obtenidas', 200)
 })
 
-router.post('/', function(req,res){
-    console.log(req.query)
-    if(req.query.error == "ok"){
-        response.error(req, res, 'algo ha sucedido al crear la tarea', 400, 'es solo una simulacion de los errores')
-    }else {
-        response.success(req, res, 'tarea creada correctamente',201)
-    }
+router.post('/', function (req, res) {
+    controller.addMessage(req.body.user, req.body.message)
+        .then((fullMessage) => {
+            response.success(req, res, fullMessage, 201)
+        })
+        .catch(e => {
+            response.error(req, res, 'algo ha sucedido al crear la tarea', 400, 'es solo una simulacion de los errores')
+        })
+
 });
 
 module.exports = router;
